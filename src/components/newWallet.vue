e75d12e882b762862f5949e954c27415d76c65b0
InvalidCastX_Labs <98199611+cryptobugnft@users.noreply.github.com>
2024-07-13 08:18:03 +0800

    Update newWallet.vue2

--- src/components/newWallet.vue
+++ src/components/newWallet.vue
@@ -1,42 +1,45 @@
 <script setup lang="ts">
-  import { ref } from "vue"
-  import { Wallet, TestNetWallet, Config } from "mainnet-js"
+import { ref } from "vue";
+import { Wallet, TestNetWallet, Config } from "mainnet-js";
+import { defineEmits } from "vue";
 
-  const seedphrase = ref(undefined as (string | undefined));
-  const selectedDerivationPath =  ref("standard" as ("standard" | "bitcoindotcom"));
-  const emit = defineEmits(['initWallet']);
+const seedphrase = ref<string | undefined>(undefined);
+const selectedDerivationPath = ref<"standard" | "bitcoindotcom" | "zapit">("standard");
+const emit = defineEmits(['initWallet']);
 
-  const nameWallet = "mywallet";
+const nameWallet = "mywallet";
 
-  async function createNewWallet() {
-    const mainnetWallet = await Wallet.named(nameWallet);
-    const walletId = mainnetWallet.toDbString().replace("mainnet", "testnet");
-    await TestNetWallet.replaceNamed(nameWallet, walletId);
-    emit('initWallet', mainnetWallet);
+async function createNewWallet() {
+  const mainnetWallet = await Wallet.named(nameWallet);
+  const walletId = mainnetWallet.toDbString().replace("mainnet", "testnet");
+  await TestNetWallet.replaceNamed(nameWallet, walletId);
+  emit('initWallet', mainnetWallet);
+}
+
+async function importWallet() {
+  let derivationPath;
+
+  if (selectedDerivationPath.value == "standard") {
+    derivationPath = "m/44'/145'/0'/0/0";
+  } else if (selectedDerivationPath.value == "zapit") {
+    derivationPath = "m/44'/245'/0'/0/0";
+  } else {
+    derivationPath = "m/44'/0'/0'/0/0";
   }
 
-  async function importWallet() {
-    //const derivationPath = selectedDerivationPath.value == "standard"? "m/44'/145'/0'/0/0" : "m/44'/0'/0'/0/0";
-    
-    let derivationPath;
-
-    if (selectedDerivationPath.value == "standard") {
-       derivationPath = "m/44'/145'/0'/0/0";
-    } else if (selectedDerivationPath.value == "zapit") {
-       derivationPath = "m/44'/245'/0'/0/0";
-    } else {
-       derivationPath = "m/44'/0'/0'/0/0";
-    }
-
-
-    if(selectedDerivationPath.value == "standard") Config.DefaultParentDerivationPath = "m/44'/145'/0'";
-    const walletId = `seed:mainnet:${seedphrase.value}:${derivationPath}`;
-    await Wallet.replaceNamed(nameWallet, walletId);
-    const walletIdTestnet = `seed:testnet:${seedphrase.value}:${derivationPath}`;
-    await TestNetWallet.replaceNamed(nameWallet, walletIdTestnet);
-    const mainnetWallet = await Wallet.named(nameWallet);
-    emit('initWallet', mainnetWallet);
+  if (selectedDerivationPath.value == "standard") {
+    Config.DefaultParentDerivationPath = "m/44'/145'/0'";
   }
+  
+  const walletId = `seed:mainnet:${seedphrase.value}:${derivationPath}`;
+  await Wallet.replaceNamed(nameWallet, walletId);
+  
+  const walletIdTestnet = `seed:testnet:${seedphrase.value}:${derivationPath}`;
+  await TestNetWallet.replaceNamed(nameWallet, walletIdTestnet);
+  
+  const mainnetWallet = await Wallet.named(nameWallet);
+  emit('initWallet', mainnetWallet);
+}
 </script>
 
 <template>
@@ -51,10 +54,11 @@
     <textarea v-model="seedphrase" style="resize: none;" rows="3" cols="50" placeholder="word1 word2 ..."></textarea>
     <span>Derivation path: </span>
     <select v-model="selectedDerivationPath">
-      <option value="zapit">m/44'/245'/0'/0’ (zapit wallet)</option>      
-      <option value="standard">m/44’/145’/0’ (standard)</option>
-      <option value="bitcoindotcom">m/44’/0’/0’ (bitcoin.com wallet)</option>
-    </select> <br>
+      <option value="zapit">m/44'/245'/0'/0' (zapit wallet)</option>      
+      <option value="standard">m/44'/145'/0' (standard)</option>
+      <option value="bitcoindotcom">m/44'/0'/0' (bitcoin.com wallet)</option>
+    </select>
+    <br>
     <input @click="importWallet()" class="button primary" type="button" style="margin-top:15px" value="Import">
     <br><br>
   </fieldset>
